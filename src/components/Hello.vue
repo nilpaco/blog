55d9e0a92389081ba77f4c575ac966a9a70e9080
Nil Panés <nil.panes@gmail.com>
2017-08-27 21:22:13 +0200

    Added http client and getStations() from api

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option v-for="station in stations" v-bind:value="station.id" v-bind:key="station.id">{{ station.name }}</option>
+    </select>
+    <select v-model="selected2">
+      <option disabled value="">Please select one</option>
+      <option v-for="station in stations" v-bind:value="station.id" v-bind:key="station.id">{{ station.name }}</option>
+    </select>
+    <span>Selected: {{ selected }} {{ selected2 }}</span>
   </div>
 </template>
 
@@ -24,15 +18,33 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      msg: 'Welcome to Your Vue.js PWA',
+      selected: '',
+      selected2: '',
+      stations: []
     }
+  },
+  methods: {
+    getStations: function () {
+      let url = 'http://barcelonaapi.marcpous.com/fgc/stations.json'
+      this.$http.get(url).then((repsonse) => {
+        this.stations = repsonse.body.data.fgc
+        console.log(this.stations)
+      }, reposen => {
+        // error
+      })
+    }
+  },
+  created () {
+    this.getStations()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
